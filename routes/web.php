1f90b3b06b2c0249a1809033188bca8d73aea4c6
yuminyu <pamama.actor@gmail.com>
2022-08-20 09:29:26 +0900

    aaa

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivewireTestController;
+use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,9 +36,7 @@ Route::middleware([
 Route::prefix('manager')
 ->middleware('can:manager-higher')
 ->group(function(){
-    Route::get('index', function () {
-        dd('manager');
-    });
+    Route::resource('events', EventController::class);
 });
 
 Route::middleware('can:user-higher')
