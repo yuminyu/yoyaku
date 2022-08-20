a4d05a0f2184198fe4c48a9232cc0b599d73b834
yuminyu <pamama.actor@gmail.com>
2022-08-20 11:29:31 +0900

    aaa

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,7 +30,7 @@ class EventController extends Controller
      */
     public function create()
     {
-        //
+        return view('manager.events.create');
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,10 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 
+        'resources/js/app.js',
+        'resources/js/flatpickr.js'
+        ])
 
         <!-- Styles -->
         @livewireStyles
